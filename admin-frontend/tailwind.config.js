211a5fb14f5c7a8b1f55ea9569d2ecffbfb9c59e
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2021-07-11 09:56:34 +0200

    feat: add theme colors

--- admin-frontend/tailwind.config.js
+++ admin-frontend/tailwind.config.js
@@ -4,11 +4,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        gray: {
+          light: "#B0B6B8", 
+          lighter: "# D5D9D8",
+          normal: "#6D7579",
+          dark: "#394249;",
+          darker: "#434C53;",
+        },
+        red: {
+          default: "#CC0033",
+          darker: "#AA142D",
+        }
+      }
     },
   },
   variants: {
